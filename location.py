de063ce538c4b50684d193806a77b479e8c79473
duckhoan.hcmut <duckhoan.work@gmail.com>
2022-01-30 18:22:42 +0700

    add get location

--- location.py
+++ location.py
@@ -1,33 +1,34 @@
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.common.by import By
-# import time
-# import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+import os
 
-# path = os.getcwd()
+path = os.getcwd()
 
-# option = webdriver.ChromeOptions()
-# option.add_argument('headless')
-# service = Service(executable_path='{}/chromedriver'.format(path))
-# browser = webdriver.Chrome(service=service)
+option = webdriver.ChromeOptions()
+option.add_argument('headless')
+service = Service(executable_path='{}/chromedriver'.format(path))
+browser = webdriver.Chrome(service=service, options=option)
 
-# def get_location():
+def get_location():
 
-#     #Truy cập trang web my-location.org để lấy tạo độ hiện tại
-#     browser.get('https://my-location.org/')
+    #Truy cập trang web my-location.org để lấy tạo độ hiện tại
+    browser.get('https://www.google.com/maps')
 
-#     #Chờ 10s để tìm vị trí
-#     time.sleep(10)
+    #Chờ 10s để tìm vị trí
+    time.sleep(5)
+    browser.find_element(By.ID, 'pWhrzc-mylocation').click()
 
-#     #Lấy tạo độ kinh tuyến và vĩ tuyến
-#     latitude = browser.find_element(By.ID, 'latitude').text
-#     longitude = browser.find_element(By.ID, 'longitude').text
+    #Lấy tạo độ kinh tuyến và vĩ tuyến
+    # latitude = browser.find_element(By.ID, 'latitude').text
+    # longitude = browser.find_element(By.ID, 'longitude').text
 
-#     return latitude, longitude
+    # return latitude, longitude
+    url = browser.current_url
+    latitude = float(url.split(',')[0].split('@')[-1])
+    longitude = float(url.split(',')[1])
+    return latitude, longitude
 
-# print(get_location())
+print(get_location())
 
-import requests
-req = requests.get('https://my-location.org/')
-
-print(req.json())
